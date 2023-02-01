c9a4e75bcd96da841e607bebb70fb68b2007a782
koda-ma <r.kodama.sys20@morijyobi.ac.jp>
2023-02-02 01:55:15 +0900

    [Fix] ログインのスタイル変更

--- login.php
+++ login.php
@@ -15,28 +15,30 @@
 </head>
 
 
-<body id="login">
-
-    <div class="contents flex-container">
+<body>
+<div class="contents flex">
+    <div class="flex-container">
         <a href="top.php" class="prev_button">←</a>
         <h2 class="login">ログイン</h2>
 
         <!--エラーが出たらここに書く⇩-->
         <p class="notice"><?php echo $error_message ?></p>
 
-        <form action="login.php">
-            <div class="login_form">
-                <div class="mail_box">
-                    <input type="text" placeholder="E-mail" name="mail">
-                </div>
-                <div class="pw_box">
-                    <input type="password" placeholder="Password" name="pw">
-                </div>
-                <input type="submit" value="ログイン">
+        <form class="login_form flex" action="login.php">
+            <div class="mail_box">
+                <input type="text" placeholder="E-mail" name="mail">
             </div>
+            <div class="pw_box">
+                <input type="password" placeholder="Password" name="pw">
+            </div>
+            <input type="submit" value="ログイン">
         </form>
-        <p>アカウントをお持ちでない方は<a href="#">コチラ</a></p>
+        <div >
+            <p>アカウントをお持ちでない方は<a href="#">コチラ</a></p>
+        </div>
     </div>
+</div>
+
 
 </body>
 </html>
