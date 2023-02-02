2c5c31b0e519499a61364fd7c1cc068d9043e401
koda-ma <r.kodama.sys20@morijyobi.ac.jp>
2023-02-02 11:24:30 +0900

    [Update] テンプレートの適用とスタイル調整

--- login.php
+++ login.php
@@ -1,44 +1,42 @@
 <?php
+    $title = "examplePage";
+    $description = "(説明)外部ファイルの読み込みテスト";
+    $is_home = true;  // ホームのページに行かないようにする
     $error_message = "";
-    // フォームの入力値がerrorならここにメッセージを格納する
-    //　$error_message = "※アカウントが存在しないかまたはパスワードが違います";
+    include 'inc/head.php'
 ?>
+<!-- 特定のページで読み取るスタイルシート等あればここに追加 -->
+<link rel="stylesheet" type="text/css" href="./css/login.css">
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="./css/login.css">
 </head>
-
-
 <body>
-<div class="contents flex">
-    <div class="flex-container">
-        <a href="top.php" class="prev_button">←</a>
-        <h2 class="login">ログイン</h2>
+<div class="wrapper">
+    <?php include 'inc/header.php'; ?>
+    <!-- 特定ページのコンテンツをここに追加 -->
 
-        <!--エラーが出たらここに書く⇩-->
-        <p class="notice"><?php echo $error_message ?></p>
+    <div class="contents flex">
+        <div class="flex-container">
+            <a href="top.php" class="prev_button">←</a>
+            <h2 class="login">ログイン</h2>
 
-        <form class="login_form flex" action="login.php">
-            <div class="mail_box">
-                <input type="text" placeholder="E-mail" name="mail">
+            <!--エラーが出たらここに書く⇩-->
+            <p class="notice"><?php echo $error_message ?></p>
+
+            <form class="login_form flex" action="login.php">
+                <div class="mail_box">
+                    <input type="text" placeholder="E-mail" name="mail">
+                </div>
+                <div class="pw_box">
+                    <input type="password" placeholder="Password" name="pw">
+                </div>
+                <input type="submit" value="ログイン">
+            </form>
+            <div >
+                <p>アカウントをお持ちでない方は<a href="#">コチラ</a></p>
             </div>
-            <div class="pw_box">
-                <input type="password" placeholder="Password" name="pw">
-            </div>
-            <input type="submit" value="ログイン">
-        </form>
-        <div >
-            <p>アカウントをお持ちでない方は<a href="#">コチラ</a></p>
         </div>
     </div>
+
 </div>
-
-
 </body>
 </html>
